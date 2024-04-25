01246349f0fe3a1f26bfcb6fb7aa898fd4ed51a2
Sefa Gür <sefa_guer@hotmail.com>
2024-04-25 14:42:25 +0200

    fixed convert funktion

--- videos/signals.py
+++ videos/signals.py
@@ -1,5 +1,5 @@
 import os
-from .tasks import convert_video
+from .tasks import convert_video, convert_video_delete
 from .models import Video
 from django.dispatch import receiver
 from django.db.models.signals import post_save,post_delete
@@ -16,3 +16,4 @@ def auto_delete_file_on_delete(sender,instance,**kwargs):
     if instance.video_file:
         if os.path.isfile(instance.video_file.path):
             os.remove(instance.video_file.path)
+            convert_video_delete(instance.video_file.path)
--- videos/tasks.py
+++ videos/tasks.py
@@ -1,9 +1,16 @@
 import os
 import subprocess
+from .models import Video
+from django.dispatch import receiver
 
 def convert_video(source):
-    target=source +'_480p.mp4'
-    print(source)
+    target = source.replace('.mp4', '_480p.mp4')
     cmd='ffmpeg -i "{}" -s hd480 -c:v libx264 -crf 23 -c:a aac -strict -2 "{}"'.format(source,target)
     subprocess.run(cmd,capture_output=True)
-    
+
+
+def convert_video_delete(source):
+    target = source.replace('.mp4', '_480p.mp4')
+    if target:
+        if os.path.isfile(target):
+            os.remove(target)
