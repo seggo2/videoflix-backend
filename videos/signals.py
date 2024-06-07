1ea4b137cfb82c1f6daf919f59cdbbcf360205ab
Sefa Gür <sefa_guer@hotmail.com>
2024-06-07 16:51:16 +0200

    test changes

--- videos/signals.py
+++ videos/signals.py
@@ -1,26 +1,19 @@
+# signals.py
 import os
-from .tasks import (
-    convert_video,
-    convert_video_delete,
-    convert_video_720p,
-    convert_video_1080p,
-    extract_thumbnail
-)
-from .models import Video
 from django.dispatch import receiver
 from django.db.models.signals import post_save, post_delete
 import django_rq
-
+from .models import Video
+from .tasks import convert_video, convert_video_720p, convert_video_1080p, extract_thumbnail, convert_video_delete
 
 @receiver(post_save, sender=Video)
 def video_post_save(sender, instance, created, **kwargs):
     if created:
-        queue = django_rq.get_queue('default', autocommit=True)
+        queue = django_rq.get_queue('default')
         queue.enqueue(convert_video, instance.video_file.path)
         queue.enqueue(convert_video_720p, instance.video_file.path)
         queue.enqueue(convert_video_1080p, instance.video_file.path)
         queue.enqueue(extract_thumbnail, instance.video_file.path)
-
 
 @receiver(post_delete, sender=Video)
 def auto_delete_file_on_delete(sender, instance, **kwargs):
