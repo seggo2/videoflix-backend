a6f55db1d54695f3f34768adf38aa52589dbee07
Sefa Gür <sefa_guer@hotmail.com>
2024-06-08 16:41:29 +0200

    debbuging

--- users/tests.py
+++ users/tests.py
@@ -10,38 +10,19 @@ class UserAPITest(APITestCase):
         )
         self.client.force_authenticate(user=self.user)
 
-    def test_user_registration(self):
-        url = reverse('custom_register')
-        data = {
-            'email': 'newuser@example.com',
-            'password1': 'newpassword123',
-            'password2': 'newpassword123',
-            'username': 'newuser'
-        }
-        response = self.client.post(url, data, format='json')
-        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
-
-    def test_user_login(self):
+    def test_login_view(self):
         url = reverse('login')
-        data = {'email': 'testuser@example.com', 'password': 'password123'}
+        data = {'username': 'testuser', 'password': 'password123'}
         response = self.client.post(url, data, format='json')
         self.assertEqual(response.status_code, status.HTTP_200_OK)
-        self.assertIn('token', response.data)
 
-    def test_user_detail(self):
+    def test_user_detail_view(self):
         url = reverse('user-detail')
         response = self.client.get(url)
         self.assertEqual(response.status_code, status.HTTP_200_OK)
-        self.assertEqual(response.data['email'], 'testuser@example.com')
+        self.assertEqual(response.data['email'], self.user.email)
 
-    def test_user_update(self):
-        url = reverse('put-view', kwargs={'pk': self.user.pk})
-        data = {'first_name': 'UpdatedFirstName'}
-        response = self.client.put(url, data, format='json')
-        self.assertEqual(response.status_code, status.HTTP_200_OK)
-        self.assertEqual(response.data['first_name'], 'UpdatedFirstName')
-
-    def test_user_delete(self):
+    def test_delete_view(self):
         url = reverse('delete-view', kwargs={'pk': self.user.pk})
         response = self.client.delete(url)
         self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
