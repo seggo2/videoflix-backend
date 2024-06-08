04869d9307ad507826a13b498fa4f98318dcd471
Sefa Gür <sefa_guer@hotmail.com>
2024-06-08 15:50:00 +0200

    debbuging

--- videoflix/settings.py
+++ videoflix/settings.py
@@ -52,6 +52,26 @@ CORS_ALLOWED_ORIGINS = [
 
 # Application definition
 
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'file': {
+            'level': 'DEBUG',
+            'class': 'logging.FileHandler',
+            'filename': 'django_debug.log',
+        },
+    },
+    'loggers': {
+        'django': {
+            'handlers': ['file'],
+            'level': 'DEBUG',
+            'propagate': True,
+        },
+    },
+}
+
+
 INSTALLED_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
